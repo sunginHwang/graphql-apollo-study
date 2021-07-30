09d2ee733d898ca2a88a31ac8c666e775c135c07
hwangseong-in <gommpo111@gmail.com>
2021-07-30 22:06:11 +0900

    study for fetching - mutations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import {
-  ApolloClient, ApolloProvider,
-  InMemoryCache,
-    gql,
+    ApolloClient, ApolloProvider,
+    InMemoryCache,
 } from '@apollo/client';
 import QueryComp from "./Query";
 import MutationComp from "./Mutation";
@@ -16,20 +14,20 @@ const cache: InMemoryCache = new InMemoryCache({});
 
 function App() {
 
-  // const { data } = useQuery(GET_GQL);
-  const client = new ApolloClient({
-    uri: 'http://localhost:3001/graphql',
-    cache
-  });
 
-  return (
-      <ApolloProvider client={client} >
-        <div className="App">
-            <QueryComp />
-            <MutationComp />
-        </div>
-      </ApolloProvider>
-  );
+    const client = new ApolloClient({
+        uri: 'http://localhost:3001/graphql',
+        cache
+    });
+
+    return (
+        <ApolloProvider client={client}>
+            <div className="App">
+                <QueryComp/>
+                <MutationComp/>
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;
